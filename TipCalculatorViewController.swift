338b9378d4a241ba0c825095da421d38e758900f
Grey Patterson <greypatterson@gmail.com>
2017-02-15 11:27:04 -0800

    Handling for digits being pressed

--- TipCalculatorViewController.swift
+++ TipCalculatorViewController.swift
@@ -19,28 +19,47 @@ class TipCalculatorViewController: UIViewController {
     @IBOutlet weak var tip: UILabel!
     @IBOutlet weak var perPerson: UILabel!
     
+    var pointPressed = false, tenCent = false, hundredCent = false
     var costD: Double { // an easier way to access the value of the cost as a double
-        get{
+        get{ // Double() can't deal with the $ in there, so strip it out and *then* cast
             let costString = cost.text!
             let ind = costString.index(costString.startIndex, offsetBy: 1)
             let outString = costString.substring(from: ind)
             return Double(outString)!
         }
-        set{
+        set{ // this part is easy, just cast to string and add the $
             let outString = "$" + String(newValue)
             cost.text = outString
         }
     }
     
-    @IBAction func digitPress(_ sender: UIButton) {
-        print(costD)
+    @IBAction func digitPress(_ sender: UIButton) { // handle a digit press
+        let digitPressed = Double(sender.title(for: .normal)!)!
+        if costD == 0 && !pointPressed{
+            costD = digitPressed
+        } else if !pointPressed {
+            costD = costD * 10
+            costD += digitPressed
+        } else if !tenCent { // we don't have a ten cent column yet, add that
+            costD = costD + 0.1 * digitPressed
+            tenCent = true
+        } else if !hundredCent{ // we don't have a cent column yet, add that
+            costD = costD + 0.01 * digitPressed
+            hundredCent = true
+        } else{
+            // do nothing
+        }
     }
     
-    @IBAction func deletePress(_ sender: UIButton) { // reset
+    @IBAction func deletePress(_ sender: UIButton) { // reset the cost to zero
         costD = 0.0
+        pointPressed = false
+        tenCent = false
+        hundredCent = false
     }
     
-    @IBAction func pointPress(_ sender: UIButton) {
+    @IBAction func pointPress(_ sender: UIButton) { // press the decimal point! oh, this is gonna be fun
+        pointPressed = true
     }
     
     
