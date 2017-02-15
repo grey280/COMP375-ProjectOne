a3246197844f1d63415c6df6b4ebac40dd6a0620
Grey Patterson <greypatterson@gmail.com>
2017-02-15 11:10:56 -0800

    Fill in all the outlets and actions for the TipCalculator

--- TipCalculatorViewController.swift
+++ TipCalculatorViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class TipCalculatorViewController: UIViewController {
     
+    func calculateTip(people: Int, amount: Double, tip: Double = 0.15) -> Double{
+        let total = amount * tip
+        return total / Double(people)
+    }
+    
+    @IBOutlet weak var cost: UILabel!
+    @IBOutlet weak var tip: UILabel!
+    @IBOutlet weak var perPerson: UILabel!
+    
+    @IBAction func digitPress(_ sender: UIButton) {
+    }
+    
+    @IBAction func deletePress(_ sender: UIButton) {
+    }
+    
+    @IBAction func pointPress(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
